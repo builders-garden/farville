e4c7789878fd046d45856d0433460a23e9bcfe10
lim0n3 <simonestaffa96@gmail.com>
2024-12-26 19:31:11 +0100

    push frame

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,27 @@
 "use client";
 
-
+import { AnimatePresence } from "framer-motion";
 import dynamic from "next/dynamic";
+import { useState } from "react";
 const GameWrapper = dynamic(() => import("./components/GameWrapper"), {
   ssr: false,
 });
 
+const WelcomeOverlay = dynamic(() => import("./components/WelcomeOverlay"), {
+  ssr: false,
+});
+
 export default function Home() {
-  //const [showWelcome, setShowWelcome] = useState(true);
+  const [showWelcome, setShowWelcome] = useState(true);
 
   return (
     <main className="min-h-screen bg-green-800">
-      {/* <AnimatePresence>
+      <AnimatePresence>
         {showWelcome && (
           <WelcomeOverlay onStart={() => setShowWelcome(false)} />
         )}
-      </AnimatePresence> */}
-      {/* {showWelcome ? null : <GameWrapper />} */}
+      </AnimatePresence>
       <GameWrapper />
     </main>
   );
 }
-
