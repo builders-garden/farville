4d1a18eafd60b6240990df5b1b2b200f8e6ca618
lim0n3 <simonestaffa96@gmail.com>
2024-12-28 11:57:56 +0100

    posthog provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,20 +3,28 @@
 import dynamic from "next/dynamic";
 import { AudioProvider } from "./context/AudioContext";
 import { GameProvider } from "./context/GameContext";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
-const WagmiProvider = dynamic(
-  () => import("./components/WagmiProvider"),
-  {
-    ssr: false,
-  }
-);
+const WagmiProvider = dynamic(() => import("./components/WagmiProvider"), {
+  ssr: false,
+});
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider>
-      <AudioProvider>
-        <GameProvider>{children}</GameProvider>
-      </AudioProvider>
-    </WagmiProvider>
+    <PostHogProvider client={posthog}>
+      <WagmiProvider>
+        <AudioProvider>
+          <GameProvider>{children}</GameProvider>
+        </AudioProvider>
+      </WagmiProvider>
+    </PostHogProvider>
   );
 }
