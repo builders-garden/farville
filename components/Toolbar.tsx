4ea1afcb26140681b61c1d1e475d8392c9548dde
lim0n3 <simonestaffa96@gmail.com>
2025-01-16 19:04:42 +0100

    padding

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -20,7 +20,7 @@ export default function Toolbar({
   return (
     <div className="fixed bottom-0 inset-x-0 bg-[#7E4E31]">
       <div
-        className="p-3 flex justify-evenly items-center"
+        className="py-3 flex justify-evenly items-center"
         style={{
           marginTop: safeAreaInsets.top,
           marginBottom: safeAreaInsets.bottom,
