7092dc7a71d295fac42ab0e1f61374bd9aa6b5b9
caso <matteocasonato@protonmail.com>
2025-02-13 10:08:52 +0100

    log request on error

--- app/api/grid-bulk/route.ts
+++ app/api/grid-bulk/route.ts
@@ -13,63 +13,73 @@ export interface GridBulkRequest {
 
 export const POST = async (req: NextRequest) => {
   const fid = req.headers.get("x-user-fid");
+
   if (!fid) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
+
   const { action, cells, itemSlug }: GridBulkRequest = await req.json();
 
-  switch (action) {
-    case "plant":
-      if (!itemSlug) {
-        return NextResponse.json(
-          { error: "Item slug is required" },
-          { status: 400 }
+  try {
+    switch (action) {
+      case "plant":
+        if (!itemSlug) {
+          return NextResponse.json(
+            { error: "Item slug is required" },
+            { status: 400 }
+          );
+        }
+        if (!Object.values(SeedType).includes(itemSlug as SeedType)) {
+          return NextResponse.json(
+            { error: "Invalid item slug" },
+            { status: 400 }
+          );
+        }
+        const plantResult = await plantBulk(
+          Number(fid),
+          cells,
+          itemSlug as SeedType
         );
-      }
-      if (!Object.values(SeedType).includes(itemSlug as SeedType)) {
-        return NextResponse.json(
-          { error: "Invalid item slug" },
-          { status: 400 }
+        return NextResponse.json({
+          success: true,
+          data: plantResult,
+        });
+      case "harvest":
+        const harvestResult = await harvestBulk(Number(fid), cells);
+        return NextResponse.json({
+          success: true,
+          data: harvestResult,
+        });
+      case "apply-perk":
+        if (!itemSlug) {
+          return NextResponse.json(
+            { error: "Item slug is required" },
+            { status: 400 }
+          );
+        }
+        if (!Object.values(PerkType).includes(itemSlug as PerkType)) {
+          return NextResponse.json(
+            { error: "Invalid item slug" },
+            { status: 400 }
+          );
+        }
+        const perkResult = await perkBulk(
+          Number(fid),
+          cells,
+          itemSlug as PerkType
         );
-      }
-      const plantResult = await plantBulk(
-        Number(fid),
-        cells,
-        itemSlug as SeedType
-      );
-      return NextResponse.json({
-        success: true,
-        data: plantResult,
-      });
-    case "harvest":
-      const harvestResult = await harvestBulk(Number(fid), cells);
-      return NextResponse.json({
-        success: true,
-        data: harvestResult,
-      });
-    case "apply-perk":
-      if (!itemSlug) {
-        return NextResponse.json(
-          { error: "Item slug is required" },
-          { status: 400 }
-        );
-      }
-      if (!Object.values(PerkType).includes(itemSlug as PerkType)) {
-        return NextResponse.json(
-          { error: "Invalid item slug" },
-          { status: 400 }
-        );
-      }
-      const perkResult = await perkBulk(
-        Number(fid),
-        cells,
-        itemSlug as PerkType
-      );
-      return NextResponse.json({
-        success: true,
-        data: perkResult,
-      });
-    default:
-      return NextResponse.json({ error: "Invalid action" }, { status: 400 });
+        return NextResponse.json({
+          success: true,
+          data: perkResult,
+        });
+      default:
+        return NextResponse.json({ error: "Invalid action" }, { status: 400 });
+    }
+  } catch (error) {
+    console.error({ action, cells, error });
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
   }
 };
