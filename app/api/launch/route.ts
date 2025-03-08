1cb71078d8d6994bb47c9d05b9c0e0875e760a48
Matteo Midena <matteo.midena@protonmail.com>
2025-03-08 18:03:03 +0100

    feat: try catch for notification launching

--- app/api/launch/route.ts
+++ app/api/launch/route.ts
@@ -3,36 +3,46 @@ import { getUsers } from "@/supabase/queries";
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  const users = await getUsers({}, [2500, 2999]);
-  // const users2 = await getUsers({}, [1000, 1999]);
-  // const users3 = await getUsers({}, [2000, 2999]);
+  const sendingResults = [];
+  try {
+    const users = await getUsers({}, [13000, 13999]);
 
-  console.log(`Sending notifications to ${users.length} users`);
+    console.log(`Sending notifications to ${users.length} users`);
 
-  const notificationsResult = [];
-  for (const user of users) {
-    console.log(`Sending notification to ${user.fid}`);
-    const result = await sendFrameNotification({
-      fid: user.fid,
-      title: "FarVille alpha is here!",
-      body: "Start farming today and compete in the global leaderboard!",
+    for (const user of users) {
+      console.log(`Sending notification to ${user.fid}`);
+      const result = await sendFrameNotification({
+        fid: user.fid,
+        title: "Brum Brum: new sharing features 🚜💨",
+        body: "You can now flex your active streak or leaderboard position on the timeline!",
+      });
+
+      sendingResults.push({
+        fid: user.fid,
+        notificationsResult: result,
+      });
+      console.log(`Notification sent to ${user.fid}`);
+    }
+
+    console.log("Notifications sent");
+  } catch (error) {
+    console.error("Error sending notifications", error);
+
+    return NextResponse.json({
+      status: "error",
+      message: "Error sending notifications",
+      notificationRecap: {
+        total: sendingResults.length,
+        results: sendingResults,
+      },
     });
-
-    notificationsResult.push({
-      fid: user.fid,
-      notificationsResult: result,
-    });
-    console.log(`Notification sent to ${user.fid}`);
   }
-
-  console.log("Notifications sent");
 
   return NextResponse.json({
     status: "ok",
     data: {
-      total: users.length,
-      users,
-      notificationsResult,
+      total: sendingResults.length,
+      results: sendingResults,
     },
   });
 }
