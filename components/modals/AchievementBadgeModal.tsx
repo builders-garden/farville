f686a6daa3fc78cf408fd64ad0ea1fec6a589ec7
Matteo Midena <matteo.midena@protonmail.com>
2025-03-10 12:54:54 +0100

    feat: new badges + badges fe

---
+++ components/modals/AchievementBadgeModal.tsx
@@ -0,0 +1,70 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+interface AchievementBadgeModalProps {
+  title: string;
+  icon?: React.ReactNode;
+  onCancel: () => void;
+  options?: {
+    titleColor?: string;
+    messageColor?: string;
+  };
+  children: React.ReactNode;
+  mintable?: boolean;
+}
+
+export default function AchievementBadgeModal({
+  title,
+  icon,
+  children,
+  onCancel,
+  options,
+  mintable,
+}: AchievementBadgeModalProps) {
+  return (
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-[60]">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.95 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.95 }}
+        className="bg-[#7E4E31] p-6 rounded-lg max-w-sm w-full mx-4 border border-[#8B5E3C]/50"
+      >
+        <div className="flex flex-row items-center gap-2 mb-4">
+          {
+            // if icon is a string, render an image
+            typeof icon === "string" ? (
+              <Image src={icon} alt={title} width={24} height={24} />
+            ) : (
+              // if icon is a React component, render it
+              icon
+            )
+          }
+          <h3
+            className={`
+            ${options?.titleColor || "text-white/90"} font-bold text-lg`}
+          >
+            {title}
+          </h3>
+        </div>
+        {children}
+        <div className="flex gap-3">
+          {mintable && (
+            <button
+              onClick={onCancel}
+              className="flex-1 py-2 px-4 rounded bg-yellow-500/50 text-white/90 transition-colors text-sm font-medium hover:bg-yellow-500/70"
+            >
+              Mint
+            </button>
+          )}
+          <button
+            onClick={onCancel}
+            className="flex-1 py-2 px-4 rounded bg-white/10 text-white/90 hover:bg-white/20 
+                     transition-colors text-sm font-medium"
+          >
+            Close
+          </button>
+        </div>
+      </motion.div>
+    </div>
+  );
+}
--- components/profile/HarvestHonour.tsx
+++ components/profile/HarvestHonour.tsx
@@ -8,6 +8,7 @@ import {
 import Image from "next/image";
 import InfoModal from "../modals/InfoModal";
 import { useState } from "react";
+import AchievementBadgeModal from "../modals/AchievementBadgeModal";
 
 interface HarvestHonourProps {
   crop: string;
@@ -34,20 +35,19 @@ export const HarvestHonour = ({
   const [badgeModalData, setBadgeModalData] = useState<BadgeModalData | null>(
     null
   );
-
-  const trophies = [1, 2, 3, 4];
-
+  const [showGoldCropModal, setShowGoldCropModal] = useState(false);
   const goldCropChancePercentage =
     step > 1
       ? BASE_GOLD_CROP_PERCENTAGE * (step - 1) * ACHIEVEMENTS_GOLD_MULTIPLIER
       : BASE_GOLD_CROP_PERCENTAGE;
+
   return (
     <Card className="bg-gradient-to-br from-[#6D4C2C] to-[#5B4120] rounded-lg border-none w-full">
       <CardContent className="flex flex-col items-center gap-2 p-4">
         <div className="flex flex-row justify-between w-full">
           {
             // render trophies
-            trophies.map(
+            [1, 2, 3, 4].map(
               (trophy) =>
                 step < trophy ? (
                   <div
@@ -64,10 +64,8 @@ export const HarvestHonour = ({
                 ) : (
                   <div
                     key={trophy}
-                    className={`h-12 w-12 bg-[#7E4E31] rounded-lg flex items-center justify-center cursor-pointer ${
-                      step === trophy
-                        ? "opacity-50 bg-[#9d5427] border-2 border-[#f2a311] border-opacity-50"
-                        : ""
+                    className={`h-12 w-12 bg-[#7E4E31] rounded-lg flex items-center justify-center border-2 border-[#f2a311] cursor-pointer ${
+                      step === trophy ? "opacity-50 border-opacity-50" : ""
                     }`}
                     onClick={() => {
                       const cropAchievements = ACHIEVEMENTS_THRESHOLDS.find(
@@ -77,9 +75,15 @@ export const HarvestHonour = ({
                         title:
                           cropAchievements?.titles[trophy - 1] ||
                           `Badge for ${crop} achievement`,
-                        description: `You demonstrated your mastery in ${crop} by harvesting ${
+                        description: `A badge issued to those who demonstrated mastery of the ${crop} by harvesting ${
                           cropAchievements?.thresholds[trophy - 1]
-                        } ${crop}!`,
+                        } ${
+                          crop.endsWith("y")
+                            ? crop.slice(0, crop.length - 1) + "ies"
+                            : crop.endsWith("o")
+                            ? crop + "es"
+                            : crop + "s"
+                        }!`,
                         badgeUrl: `/images/badge/${crop}-${trophy}.png`,
                         step: trophy,
                       });
@@ -129,7 +133,10 @@ export const HarvestHonour = ({
           </div>
 
           {/* gold crop percentage value */}
-          <div className="flex flex-col items-center justify-center">
+          <div
+            className="flex flex-col items-center justify-center cursor-pointer"
+            onClick={() => setShowGoldCropModal(true)}
+          >
             <Image
               src="/images/special/gold.png"
               alt="Gold ingot"
@@ -140,15 +147,61 @@ export const HarvestHonour = ({
               {goldCropChancePercentage * 100}%
             </p>
           </div>
+          {showGoldCropModal && (
+            <InfoModal
+              title="Gold Crop Chance"
+              icon="/images/special/gold.png"
+              onCancel={() => setShowGoldCropModal(false)}
+            >
+              <div className="flex flex-col items-center gap-2 my-8">
+                <p className="text-xs text-white/90">
+                  The chance of harvesting a gold crop increases with each
+                  achievement level.
+                </p>
+                <p className="text-xs text-white/90">
+                  The base chance is{" "}
+                  <span className="text-yellow-400 font-bold">
+                    {BASE_GOLD_CROP_PERCENTAGE * 100}%
+                  </span>
+                  , and each achievement level increases it by{" "}
+                  <span className="text-yellow-400 font-bold">
+                    {BASE_GOLD_CROP_PERCENTAGE *
+                      ACHIEVEMENTS_GOLD_MULTIPLIER *
+                      100}
+                    %
+                  </span>
+                  .
+                </p>
+                <Card className="bg-gradient-to-br from-[#6D4C2C] to-[#5B4120] rounded-lg border-none w-full mt-4">
+                  <CardContent className="flex flex-row text-white/90 justify-between items-center p-4">
+                    <div className="relative w-12 h-12">
+                      <Image
+                        src={`/images/crop/gold-${crop}.png`}
+                        alt={`Gold ${crop}`}
+                        layout="fill"
+                      />
+                    </div>
+                    <p className="w-4/5 text-xs">
+                      Your current chance to find a gold {crop} is{" "}
+                      <span className="text-yellow-400 font-bold">
+                        {goldCropChancePercentage * 100}%
+                      </span>
+                    </p>
+                  </CardContent>
+                </Card>
+              </div>
+            </InfoModal>
+          )}
         </div>
 
         {badgeModalData && (
-          <InfoModal
+          <AchievementBadgeModal
             title={`${crop[0].toUpperCase() + crop.slice(1)} #${
               badgeModalData.step
             }`}
             icon={`/images/crop/${crop}.png`}
             onCancel={() => setBadgeModalData(null)}
+            mintable={badgeModalData.step < step}
           >
             <div className="flex flex-col items-center gap-2">
               <div className="relative w-52 h-52 rounded-lg my-4 border-4 border-[#f2a311]">
@@ -166,7 +219,7 @@ export const HarvestHonour = ({
                 {badgeModalData.description}
               </p>
             </div>
-          </InfoModal>
+          </AchievementBadgeModal>
         )}
       </CardContent>
     </Card>
