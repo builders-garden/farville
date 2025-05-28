6e985bd02c6f523a5c253d63341f69368fad7130
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2025-05-28 13:03:34 +0200

    add react-scan

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import type { Metadata } from "next";
 import { Press_Start_2P } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { ReactScan } from "@/components/ReactScan";
 
 const pixelFont = Press_Start_2P({
   weight: "400",
@@ -19,10 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-    >
+    <html lang="en" suppressHydrationWarning>
+      <ReactScan />
       <body className={`${pixelFont.className} antialiased`}>
         <Providers>{children}</Providers>
       </body>
